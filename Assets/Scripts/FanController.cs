6441f4173c6a7bb278e92ee830ff5a487ae31275
GadConnorHowell <46717327+GadConnorHowell@users.noreply.github.com>
2019-03-01 16:50:50 +0000

    Initial commit

---
+++ Assets/Scripts/FanController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanController : MonoBehaviour {
+
+	public ButtonController button ; 
+	public AreaEffector2D fan;
+	public GameObject air;
+	private bool spawnAir;
+	public float airCount ; 
+	public float airR = 0.6f ;
+	// Use this for initialization
+	void Start () {
+		fan = gameObject.GetComponent<AreaEffector2D> ();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (button.fanOn == true) {
+
+
+			fan.forceMagnitude = 15; 
+			spawnAir = true; 
+		}
+
+		if (spawnAir == true && airCount <= 0) {
+
+			StartCoroutine (AirSpawn());
+
+		}
+		air.transform.position = new Vector2 (2, -5);
+		air.transform.rotation = Quaternion.Euler (0, 0, 0.6f);
+	}
+
+
+	IEnumerator AirSpawn () 
+	{
+
+		Instantiate (air);
+		yield return new WaitForSeconds (0.1f);
+		airCount += 1; 
+	
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed ;
+	Rigidbody2D rb2d ;
+	public float jumpForce;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = gameObject.GetComponent<Rigidbody2D> (); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A)) {
+
+			gameObject.transform.Translate (Vector2.left * speed * Time.deltaTime);
+
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+
+			gameObject.transform.Translate (Vector2.right * speed * Time.deltaTime);
+
+		}
+
+		if (Input.GetButtonDown ("Jump")) {
+			Vector2 jf = new Vector2 (0, jumpForce);
+
+			rb2d.AddForce (jf ,ForceMode2D.Impulse) ;
+
+		}
+
+	}
+
+
+}
